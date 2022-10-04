f60bd22782b0103cce410c6b10e0e4dfa1b9dc76
Alim Kerem Erdoğmuş <akerdogmus@gmail.com>
2022-10-04 15:53:26 +0300

    Update start_system_node.py

--- srvt_moveit/src/start_system_node.py
+++ srvt_moveit/src/start_system_node.py
@@ -9,7 +9,7 @@ import rospy
 def main_func():
     """main func"""
     rospy.init_node('start_system_node', anonymous=True)
-    os.system("gnome-terminal -x roslaunch srvt_gazebo start_rokos.launch")
+    os.system("gnome-terminal -x roslaunch srvt_gazebo start_otokar.launch")
 
     time.sleep(10)
     os.system("gnome-terminal -x roslaunch srvt_moveit start_rokos.launch")
